7acaf516aa2bd006711f3e183f7ff5aa7a194c13
sagar <ss3473@njit.edu>
2019-08-03 09:43:59 -0400

    just a test.

--- Public/index.php
+++ Public/index.php
@@ -6,5 +6,36 @@
  * Time: 1:11 AM
  */
 
-echo 'test';
-echo 'test2';
+main::start();
+
+class main {
+
+    static public function start() {
+
+        $file = fopen("sagar.csv","r");
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+
+            $records[] =$record;
+        }
+
+        fclose($file);
+        Print_r ($records);
+    }
+   }
+
+echo "<html><body><table>\n\n";
+$f = fopen("sagar.csv", "r");
+while (($line = fgetcsv($f)) !== false) {
+    echo "<tr>";
+    foreach ($line as $cell) {
+        echo "<td>" . htmlspecialchars($cell) . "</td>";
+    }
+    echo "</tr>\n";
+}
+fclose($f);
+echo "\n</table></body></html>";
+
+main::start("sagar.csv");
